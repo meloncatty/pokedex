0daa46e05be2c129fcd5264017b6a27765dfea59
meloncatty <k.handel66@gmail.com>
2018-05-31 11:07:49 -0600

    Begin fetching pokemon by category

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,11 @@ export const typesSuccess = (typesList) => ({
   typesList
 })
 
+export const categorizedMons = (mons) => ({
+  type: 'CATEGORIZED_MONS',
+  specificTypes: mons
+})
+
 export function typeThunk() {
   return (dispatch) => {
     dispatch(typesAreLoading(true))
@@ -27,6 +32,24 @@ export function typeThunk() {
       })
       .then(response => response.json())
       .then(types => dispatch(typesSuccess(types)))
+      .catch(() => dispatch(typesHaveErrored(true)))
+  }
+}
+
+export function categorizeThunk(id) {
+  return (dispatch) => {
+    dispatch(typesAreLoading(true))
+    const url = 'http://localhost:3001/types${id}'
+    return fetch(url)
+      .then(response => {
+        if(!response.ok) {
+          throw Error(response.statusText)
+        }
+        dispatch(typesAreLoading(false))
+        return response
+      })
+      .then(response => response.json())
+      .then(mons => dispatch(categorizedMons(mons)))
       .catch(() => dispatch(typesHaveErrored(true)))
   }
 }
