6fb5dccea6cbee177ddf881abcf8cda55c38dd1b
meloncatty <k.handel66@gmail.com>
2018-05-31 10:45:04 -0600

    Render types to page Update styles

--- src/containers/FakeContainer/index.js
+++ src/containers/FakeContainer/index.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import PropTypes, { shape, func, string } from 'prop-types';
-import { connect } from 'react-redux';
-import { typeThunk } from '../../actions';
+import React, { Component } from 'react'
+import PropTypes, { shape, func, string } from 'prop-types'
+import { connect } from 'react-redux'
+import { typeThunk } from '../../actions'
+import loadingAnimation from '../../assets/pikaWave.gif'
+
 class FakeContainer extends Component {
+
+  componentDidMount() {
+    this.props.typeThunk()
+  }
+
+  makeTypeCards() {
+    return this.props.typesSuccess.map(type =>
+      <div>{type.name}</div>
+    )
+  }
 
   render() {
     return (
       <div>
-        <button onClick={() => { this.props.typeThunk() }}>click me</button>
+        <section className='card-container'>
+        {this.makeTypeCards()}
+        </section>
       </div>
-    );
+    )
   }
 }
 
 FakeContainer.propTypes = {
-  fake: shape({ fake: string }),
-  fakeAction: func.isRequired
-};
+  typeThunk: func.isRequired
+}
 
-const mapStateToProps = state => ({  });
+const mapStateToProps = state => ({
+  isLoading: state.isLoading,
+  typesSuccess: state.typesSuccess
+})
 
 const mapDispatchToProps = dispatch => ({
   typeThunk: () => dispatch(typeThunk())
 })
-export default connect(mapStateToProps, mapDispatchToProps)(FakeContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(FakeContainer)
